3cda2c940fbd43b8f47d1fcf365b418ea9c1513b
JazzyLu <44308522+JazzyLu@users.noreply.github.com>
2021-03-22 01:21:06 +0100

    Add files via upload

---
+++ subjects.js
@@ -0,0 +1,92 @@
+let subjects = [
+    {
+        id: 1,
+        title: "Biology",
+        location: "Valleta",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 2,
+        title: "Science",
+        location: "Rabat",
+        price: 12000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 3,
+        title: "English",
+        location: "Valleta",
+        price: 11000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 4,
+        title: "Algebra",
+        location: "Mdina",
+        price: 12000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 5,
+        title: "Economics",
+        location: "Zebbug",
+        price: 9000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 6,
+        title: "Physical Education",
+        location: "Valleta",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 7,
+        title: "French",
+        location: "Rabat",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 8,
+        title: "Fine Art",
+        location: "Valleta",
+        price: 10500,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 9,
+        title: "Geography",
+        location: "Mdina",
+        price: 8500,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 10,
+        title: "History",
+        location: "Zebbug",
+        price: 9500,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    }
+]
